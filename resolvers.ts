0f945f89cc084e70796504078e813e6a9eb81e58
Ghislain Thau <ghislain.thau@gmail.com>
2022-01-12 20:18:37 +0100

    modified resolvers to get query resolvers signatures

--- resolvers.ts
+++ resolvers.ts
@@ -1,5 +1,8 @@
-export const queryResolvers = {
+import { Resolvers } from './generated/graphql';
+
+export const queryResolvers: Resolvers = {
   Query: {
     hello: () => 'World',
+    queryWithArgs: (_, {arg1, arg2}, context) => `Hello ${arg1} ${arg2 ?? 1}`,
   },
 };
