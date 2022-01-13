2ca2ce6659476a727d652afa3943bbfb8ace6fc8
Ghislain Thau <ghislain.thau@gmail.com>
2022-01-13 11:40:14 +0100

    use graphql-yoga instead of more complex fastify+envelop

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,9 @@
-import { envelop, useLogger, useSchema, useTiming } from '@envelop/core';
-import { useParserCache } from '@envelop/parser-cache';
-import { useValidationCache } from '@envelop/validation-cache';
-import { loadFilesSync } from "@graphql-tools/load-files";
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { fastify } from 'fastify';
-import cors from 'fastify-cors';
-import { GraphQLError } from 'graphql';
-import { join } from 'path';
-import { graphqlHandler } from './graphql-http-handler';
-import { queryResolvers } from './resolvers';
+import { GraphQLServer } from 'graphql-yoga';
+import { queryResolvers as resolvers } from './resolvers/resolvers';
 
+const server = new GraphQLServer({
+  typeDefs: './src/typedefs.graphql',
+  resolvers,
+} as any);
 
-export async function main() {
-  const server = fastify({
-    disableRequestLogging: true,
-    logger: {
-      level: 'debug',
-    },
-  });
-
-  process
-    .on('unhandledRejection', (reason, p) => {
-      server.log.error(reason as any, 'Unhandled Rejection at Promise', p);
-    })
-    .on('uncaughtException', (err: GraphQLError) => {
-      server.log.error(err as any, 'Uncaught Exception thrown');
-    });
-
-  server.register(cors, {
-    origin: true,
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['x-requested-with', 'Content-type'],
-    credentials: true,
-  });
-
-  try {
-    const graphqlPath = '/graphql';
-    const subscriptionsPath = '/subscriptions';
-    let playgroundGraphqlEndpoint = graphqlPath;
-    let playgroundSubscriptionEndpoint = subscriptionsPath;
-
-    const port = process.env.PORT || 4000;
-
-    const schema = makeExecutableSchema({
-      typeDefs: loadFilesSync(join(__dirname, './typedefs.graphql')),
-      resolvers: {
-        ...queryResolvers,
-      },
-    });
-
-    const getEnveloped = envelop({
-      plugins: [
-        useSchema(schema),
-        useLogger(),
-        useTiming(),
-        useParserCache(),
-        useValidationCache(),
-      ],
-    });
-
-    server.route({
-      method: ['GET', 'POST'],
-      url: graphqlPath,
-      handler: await graphqlHandler({
-        getEnveloped,
-        graphiqlEndpoint: playgroundGraphqlEndpoint,
-        subscriptionsEndpoint: playgroundSubscriptionEndpoint,
-      }),
-    });
-
-    await server.listen(port, '0.0.0.0');
-  } catch (error) {
-    server.log.fatal(error);
-    process.exit(1);
-  }
-}
-
-main().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+server.start(() => console.log('Server is running on http://localhost:4000'))
--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -1,4 +1,4 @@
-import { Resolvers } from './generated/graphql';
+import { Resolvers } from '../generated/graphql';
 
 export const queryResolvers: Resolvers = {
   Query: {
