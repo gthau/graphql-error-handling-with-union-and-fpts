8e6dfe2247407c5a11210fa8fa4ffff84413d034
Ghislain Thau <ghislain.thau@gmail.com>
2022-01-13 19:47:18 +0100

    schema and query for entities, and codegen mapping

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -1,8 +1,25 @@
 import { Resolvers } from '../generated/graphql';
+import { getEntity, isUserAllowedForEntity, getUser } from '../data/service';
 
 export const queryResolvers: Resolvers = {
   Query: {
     hello: () => 'World',
-    queryWithArgs: (_, {arg1, arg2}, context) => `Hello ${arg1} ${arg2 ?? 1}`,
+
+    entity: (_, args, __) => {
+      const { id: entityId, userId } = args;
+      const entity = getEntity(Number(entityId));
+      const user = getUser(Number(userId));
+
+      return (!entity || !user)
+        ? null
+        : isUserAllowedForEntity(user, entity)
+          ? entity
+          : null;
+    }
   },
+
+  Entity: {
+    id: (parent) => String(parent.id),
+    name: (parent) => parent.name,
+  }
 };
