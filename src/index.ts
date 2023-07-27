1d859453e7d58a35400836f38ccc526d2d8a4307
Ghislain Thau <ghislain.thau@gmail.com>
2023-07-27 18:53:38 +0200

    Modified Graphql Yoga instantiation after upgrade to v4

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { loadSchema } from '@graphql-tools/load';
+import { addResolversToSchema } from '@graphql-tools/schema';
+import { createYoga } from 'graphql-yoga';
+import { createServer } from 'http';
 import { queryResolvers as resolvers } from './resolvers/resolvers';
 
-const server = new GraphQLServer({
-  typeDefs: './src/typedefs.graphql',
-  resolvers,
-  resolverValidationOptions: {
-    requireResolversForResolveType: false,
-  }
-} as any);
+async function main() {
+  const schema = await loadSchema('./src/typedefs.graphql', {
+    loaders: [new GraphQLFileLoader()],
+  });
+  const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+  const yoga = createYoga({
+    schema: schemaWithResolvers,
+  });
+  const server = createServer(yoga);
+
+  server.listen(4000, () => {
+    console.log('Server is running on http://localhost:4000');
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
